7227db63ec103a1f8cc0ab2fa8dc465fe2fd4f4a
Simanta <simantaray89@gmail.com>
2022-02-15 20:10:12 +0530

    add: cors implementation

--- ecommerce_backend/index.js
+++ ecommerce_backend/index.js
@@ -1,17 +1,28 @@
 const express       = require("express")
 const mongoose      = require("mongoose");
 const dotenv        = require("dotenv");
-const app           = express();
+const cors          = require('cors')
 const userRoute     = require("./routes/user")
 const authRoute     = require("./routes/auth")
 const productRouter = require("./routes/product")
 const cartRouter    = require("./routes/cart")
+
+const app           = express();
 dotenv.config();
 app.use(express.json());
 
 app.listen(process.env.PORT || 5000,()=>{
   console.log("listing");
 })
+app.use(cors())
+// app.use(cors(), function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "http://localhost:5000"); // update to match the domain you will make the request from
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 
 mongoose.connect(process.env.MONGO_LINK)
 .then(()=>{

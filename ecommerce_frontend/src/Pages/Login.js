9602e394d054c588ac8475c289708988baddf396
Simanta <simantaray89@gmail.com>
2022-02-16 21:02:42 +0530

    add: login to Homepage

--- ecommerce_frontend/src/Pages/Login.js
+++ ecommerce_frontend/src/Pages/Login.js
@@ -1,35 +1,44 @@
+import { useState } from "react";
 import styled from "styled-components";
-import axios from 'axios';
-import {mobile} from "../responsive";
-import { useSelector, useDispatch } from 'react-redux'
-import { loginStart, loginSuccess , loginFailure} from '../redux/userSlice'
+import axios from "axios";
+import { mobile } from "../responsive";
+import { useSelector, useDispatch } from "react-redux";
+import { loginStart, loginSuccess, loginFailure } from "../redux/userSlice";
 const Login = () => {
   //const logingdata=useSelector((state)=>state.user.currentUser)
-  const dispatch = useDispatch()
-  
-const fetchTest= ()=>{
-  console.log("hi");
-  axios.get("https://jsonplaceholder.typicode.com/users/1")
-  .then(response => response.data)
-  .then(json => console.log(json))
-  .catch(err=>{
-    console.log(err);
-  })
-}
-
+  const dispatch = useDispatch();
+  const [userName,SetUserName]   =useState("");
+  const [email,SetEmail]         =useState("");
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    
+    const data = {
+      username: userName,
+      password: email,
+    };
+    console.log(data);
+    axios
+      .post("http://localhost:5000/api/auth/login", data)
+      .then((response) => {
+        console.log(response.data);
+        dispatch(loginSuccess(response.data.username));
+      })
+      // .then(json => console.log(json))
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
-
     <Container>
-      {fetchTest()}
       <Wrapper>
         <Title>SIGN IN</Title>
-        <Form>
-          <Input placeholder="username" />
-          <Input placeholder="password" />
-          <Button onClick={()=>dispatch(loginSuccess())}>LOGIN</Button>
-          <Link>DO NOT YOU REMEMBER THE PASSWORD?</Link>
-          <Link>CREATE A NEW ACCOUNT</Link>
+        <Form onSubmit={handleSubmit}>
+          <Input placeholder="username" onChange={event => SetUserName(event.target.value)} />
+          <Input placeholder="email" onChange={event => SetEmail(event.target.value)} />
+          <Button>LOGIN</Button>
+          {/* <Link>DO NOT YOU REMEMBER THE PASSWORD?</Link>
+          <Link>CREATE A NEW ACCOUNT</Link> */}
         </Form>
       </Wrapper>
     </Container>
@@ -38,38 +47,34 @@ const fetchTest= ()=>{
 
 export default Login;
 
-
-
-
 //styles
 
-const Container= styled.div`
-background-color: rgba(0,0,0,.9);
+const Container = styled.div`
+  background-color: rgba(0, 0, 0, 0.9);
   width: 100vw;
   height: 100vh;
   display: flex;
   align-items: center;
   justify-content: center;
   position: absolute;
-  top:0;
+  top: 0;
   z-index: 100;
-  `;
+`;
 const Wrapper = styled.div`
-    z-index: 10;
+  z-index: 10;
   width: 25%;
   padding: 20px;
-  background-color: rgb(245,245,245);
+  background-color: rgb(245, 245, 245);
   ${mobile({ width: "75%" })}
   border-radius: 10px;
-  box-shadow: rgba(0, 0, 0, 0.1) 0px 20px 25px -5px, rgba(0, 0, 0, 0.04) 0px 10px 10px -5px;
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 20px 25px -5px,
+    rgba(0, 0, 0, 0.04) 0px 10px 10px -5px;
 `;
 
 const Title = styled.h1`
   font-size: 20px;
   font-weight: 500;
-  padding-left:55px;
-
-
+  padding-left: 55px;
 `;
 
 const Form = styled.form`
@@ -86,7 +91,6 @@ const Input = styled.input`
 `;
 
 const Button = styled.button`
-  
   width: 75%;
   border: none;
   padding: 12px 20px;
@@ -102,8 +106,7 @@ const Button = styled.button`
 const Link = styled.a`
   margin: 5px 0px;
   font-size: 14px;
-  font-weight:bold;
- color: rgb(40, 116, 240);
+  font-weight: bold;
+  color: rgb(40, 116, 240);
   cursor: pointer;
 `;
-

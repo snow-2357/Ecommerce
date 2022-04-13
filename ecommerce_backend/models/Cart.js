1e9a3a8a0a91613c961e0742a11b17b6c2de8e9a
simantaray <simantaray89@gmail.com>
2022-04-13 17:02:37 +0530

    fixed cart creation/ add itemsin cart

--- ecommerce_backend/models/Cart.js
+++ ecommerce_backend/models/Cart.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const CartSchema = new mongoose.Schema(
   {
-    userId: { type: String, required: true },
+    userId: { type: String, required: true ,unique: true},
     products: [
       {
         productId: {
--- ecommerce_backend/routes/cart.js
+++ ecommerce_backend/routes/cart.js
@@ -1,18 +1,30 @@
 const router = require("express").Router();
 const Cart = require("../models/Cart");
 
+//create cart for a user
+
+router.post("/:id", async (req,res)=>{
+  const newCart =new Cart({userId: req.params.id, "products":[]})
+  try {
+    const savedCart=await newCart.save();
+    res.status(200).json(savedCart);
+} catch (error) {
+    res.status(500).json(error);      
+}
+})
+
 // add new item in cart
-  router.post("/:id", async (req, res) => {
-    try {
-      const updatedCart = await Cart.findOneAndUpdate(
-      {userId: req.params.id},{$push:{"products":{"productId":121,"quantity":1}}},{new:true}
-      );
-      res.status(200).json(updatedCart);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
   
+router.post("/add/:id", async (req, res) => {
+  try {
+    const updatedCart = await Cart.findOneAndUpdate(
+    {userId: req.params.id},{$push:req.body},{new:true}
+    );
+    res.status(200).json(updatedCart);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
   //GET USER CART
   router.get("/find/:userId",  async (req, res) => {
